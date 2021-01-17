d13f3c7860a5b5cd17fffb72307a4b622edc7282
RealBenMiller <31767890+RealBenMiller@users.noreply.github.com>
2021-01-17 15:24:26 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3d-room",
+      "name": "3d Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.5099564560529082,
+          "pitch": -0.09401725164200059,
+          "title": "Welcome",
+          "text": "This man says hello"
+        },
+        {
+          "yaw": 1.0657019553361629,
+          "pitch": 0.04260144511981423,
+          "title": "We could put something nice here",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.07011008841626776,
+          "pitch": -0.5899194143226154,
+          "title": "View of Canary Wharf",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
